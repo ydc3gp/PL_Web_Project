6272b7517bc69a3942e0c65299c3a9ee0b93679b
Andrew Abbott <ydc3gp@virginia.edu>
2025-03-31 23:37:21 -0400

    changed config.php back to normal

--- includes/config.php
+++ includes/config.php
@@ -1,22 +1,23 @@
 <?php
-/**
- * Configuration information
- * CS4640 Spring 2024
- *
- * Contains connection information for the local Docker
- * PostgresSQL database.  When uploading your code to the
- * CS4640 server, you can replace this file with another
- * configuration containing connection information found
- * on our course Canvas site.
- */
+ /**
+  * Configuration information
+  * CS4640 Spring 2024
+  *
+  * Contains connection information for the local Docker
+  * PostgresSQL database.  When uploading your code to the
+  * CS4640 server, you can replace this file with another
+  * configuration containing connection information found
+  * on our course Canvas site.
+  */
+ 
+ 
+ class Config {
+     public static $db = [
+         "host" => "localhost",
+         "port" => 5432,
+         "user" => "localuser",
+         "pass" => "cs4640LocalUser!",
+         "database" => "example"
+     ];
+ }
 
-
-class Config {
-    public static $db = [
-        "host" => "localhost",
-        "port" => 5432,
-        "user" => "ydc3gp",
-        "pass" => "BiEUL4933ZgA",
-        "database" => "ydc3gp"
-    ];
-}
